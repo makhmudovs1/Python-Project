1f0607431ea99ead7d100eea1abdd62845ef6c74
Sheravgan <makhmudov.shb@gmail.com>
2023-11-11 23:27:54 +0300

    add files

--- pythonProject2/main.py
+++ pythonProject2/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import filedialog
 from tkinter import font
+from tkinter import simpledialog
 import os
 
 root = Tk()
@@ -30,7 +31,9 @@ def open_file():
     my_text.delete("1.0", END)
 
     # Grab Filename
-    text_file = filedialog.askopenfilename(initialdir="", title="Open File", filetypes=(("Text Files", "*.txt"), ("HTML Files", "*html"), ("Python Files", "*,py"), ("All Files", "*.*")))
+    text_file = filedialog.askopenfilename(initialdir="", title="Open File",
+                                           filetypes=(("Text Files", "*.txt"), ("HTML Files", "*html"),
+                                                      ("Python Files", "*.py"), ("All Files", "*.*")))
 
     # Check to see if there is a file
     if text_file:
@@ -74,33 +77,46 @@ def save_as_file():
 
 
 # Save File
-def save_file():
+def save_file(event=None):
     global open_status_name
     if open_status_name:
         # Save the file
         text_file = open(open_status_name, 'w')
         text_file.write(my_text.get(1.0, END))
-        # Close The File
         text_file.close()
-        # Update Status Bar
         status_bar.config(text=f'Saved: {open_status_name}        ')
     else:
         # Prompt user to save the file with a new name
-        file_name = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
+        file_name = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt"),
+                                                                                    ("All files", "*.*")])
         if file_name:
             open_status_name = file_name
-            # Save the file with the new name
             text_file = open(open_status_name, 'w')
             text_file.write(my_text.get(1.0, END))
-            # Close The File
             text_file.close()
-            # Update Status Bar
             status_bar.config(text=f'Saved: {open_status_name}        ')
 
 
 # Exit
-def exit_app():
+def exit_app(event=None):
     root.quit()
+
+
+# Function to delete selected text
+def delete_text(event=None):
+    my_text.delete(1.0, END)
+
+
+# Function to find and replace text
+def find_and_replace(event=None):
+    find_string = simpledialog.askstring("Find", "Find:")
+    if find_string:
+        replace_string = simpledialog.askstring("Replace", f"Replace {find_string} with:")
+        if replace_string:
+            content = my_text.get(1.0, END)
+            content = content.replace(find_string, replace_string)
+            my_text.delete(1.0, END)
+            my_text.insert(INSERT, content)
 
 
 # Create main Frame
@@ -112,8 +128,8 @@ text_scroll = Scrollbar(my_frame)
 text_scroll.pack(side=RIGHT, fill=Y)
 
 # Create Text Box
-
-my_text = Text(my_frame, width=97, height=25, font=("Helvetica",16), selectbackground="yellow", selectforeground="black", undo=True, yscrollcommand=text_scroll.set)
+my_text = Text(my_frame, width=97, height=25, font=("Helvetica", 16), selectbackground="yellow",
+               selectforeground="black", undo=True, yscrollcommand=text_scroll.set)
 my_text.pack()
 
 # Configure our Scrollbar
@@ -141,9 +157,23 @@ edit_menu.add_command(label="Copy", command=lambda: my_text.event_generate("<<Co
 edit_menu.add_command(label="Paste", command=lambda: my_text.event_generate("<<Paste>>"))
 edit_menu.add_command(label="Undo", command=lambda: my_text.event_generate("<<Undo>>"))
 edit_menu.add_command(label="Redo", command=lambda: my_text.event_generate("<<Redo>>"))
+edit_menu.add_separator()
+edit_menu.add_command(label="Delete", command=delete_text)
+edit_menu.add_separator()
+edit_menu.add_command(label="Find and Replace", command=find_and_replace)
 
 # Add Status Bar To Bottom Of App
 status_bar = Label(root, text='Ready         ', anchor=E)
 status_bar.pack(fill=X, side=BOTTOM, ipady=5)
+
+# Bind hotkeys
+root.bind("<Control-s>", save_file)
+root.bind("<Control-S>", save_file)
+root.bind("<Control-q>", exit_app)
+root.bind("<Control-Q>", exit_app)
+root.bind("<Control-d>", delete_text)
+root.bind("<Control-D>", delete_text)
+root.bind("<Control-f>", find_and_replace)
+root.bind("<Control-F>", find_and_replace)
 
 root.mainloop()
